cdf5daa8f31a90b3e64945c3801f80a534585acb
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-05-05 18:53:50 +0200

    change font

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "SourceCodePro:size=9" };
+static const char dmenufont[]       = "SourceCodePro:size=9";
 // background
 static const char col_gray1[]       = "#222222";
 // inactive border
