c11e399a70daa5bad56cc03312ec370189cd7dca
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-05-08 16:09:43 +0200

    add better screenshot handling

--- config.h
+++ config.h
@@ -60,7 +60,8 @@ static const Layout layouts[] = {
 
 /* commands */
 
-static const char *printscreen[] = {"scrot", "%Y-%m-%d_$wx$h.png", "-e", "mv $f ~/shots", NULL };
+static const char *printscreen[] = {"flameshot", "full", "-c", NULL};
+static const char *printscreen_select[] = {"flameshot", "gui", NULL};
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
@@ -68,7 +69,8 @@ static const char *termcmd[]  = { "st", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
     { 0,                            XK_Print,  spawn,          {.v = printscreen } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+    { ShiftMask,                    XK_Print,  spawn,          {.v = printscreen_select } },
+    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             		    XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
